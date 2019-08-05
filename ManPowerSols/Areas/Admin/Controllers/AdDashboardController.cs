6462d193f36f875d7744233d4f7b81f944cda69a
prathap.wgl@gmail.com <prathap.wgl@gmail.com>
2019-08-05 10:42:12 -0400

    Included untracked files into the build

---
+++ ManPowerSols/Areas/Admin/Controllers/AdDashboardController.cs
@@ -0,0 +1,166 @@
+﻿using ManPowerSols.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Data;
+using ManPowerSols.Core.Models;
+using Newtonsoft.Json;
+using ManPowerSols.Core;
+
+namespace ManPowerSols.Areas.Admin.Controllers
+{
+    public class AdDashboardController : Controller
+    {
+        // GET: Admin/AdDashboard
+
+        AdminRepository arobj = new AdminRepository();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            return View();
+        }
+        public ActionResult EmpJobs()
+        {
+            return View();
+        }
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        public ActionResult CreateAdmin()
+        {
+            return View();
+        }
+        public ActionResult AdminDeals()
+        {
+            return View();
+        }
+
+        public JsonResult GetRegsiteredUsers()
+        {
+            var res = "";
+            try
+            {
+                string jsr = arobj.GetRegsiteredUsers();
+                return Json((new JavaScriptSerializer()).DeserializeObject(jsr), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                res = ex.Message.ToString();
+                return Json(res, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult ApproveUser(ApproveUser au)
+        {
+            var res = "";
+            try
+            {
+                string jsr = arobj.ApproveUser(au);
+                return Json((new JavaScriptSerializer()).DeserializeObject(jsr), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                res = ex.Message.ToString();
+                return Json(res, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult UpdateJobAccess(UpdateAccess ua)
+        {
+            var res = "";
+            try
+            {
+                string jsr = arobj.UpdateJobAccess(ua);
+                return Json((new JavaScriptSerializer()).DeserializeObject(jsr), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                res = ex.Message.ToString();
+                return Json(res, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult GetEmpPostsbyStatus(UpdateAccess ua)
+        {
+            var res = "";
+            try
+            {
+                string jsr = arobj.GetEmpPostsbyStatus(ua);
+                return Json((new JavaScriptSerializer()).DeserializeObject(jsr), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                res = ex.Message.ToString();
+                return Json(res, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult GetEmpAgentDeals(string usrid, string rectype)
+        {
+            var res = "";
+            try
+            {
+                string jsr = arobj.GetEmpAgentDeals(usrid, rectype);
+                return Json((new JavaScriptSerializer()).DeserializeObject(jsr), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                res = ex.Message.ToString();
+                return Json(res, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public JsonResult UpdateAppliedJob(JobApplyModel jm)
+        {
+            var res = "";
+            try
+            {
+                string jsr = arobj.UpdateAppliedJob(jm);
+                return Json((new JavaScriptSerializer()).DeserializeObject(jsr), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                res = ex.Message.ToString();
+                return Json(res, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ChangePassword(FormCollection frm)
+        {
+            ChangePasswordModel cpm = new ChangePasswordModel();
+            MainRepoistory mps = new MainRepoistory();
+            try
+            {
+                cpm.userid = frm["txtuserid"];
+                cpm.usertype = frm["txtusertype"];
+                cpm.password = frm["txtcurrentpwd"];
+                cpm.newpassword = frm["txtnewpwd"];
+                string res = mps.ChangePassword(cpm);
+                JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+                var finalres = JsonConvert.DeserializeObject<ChangePasswordResult>(res);
+                TempData["ResponseCode"] = finalres.ResponseCode;
+                TempData["ResponseMessage"] = finalres.ResponseMessage;
+            }
+            catch (Exception ex)
+            {
+                ErrorWriter.WriteLog(ex.GetType().ToString(), ex.GetType().Name.ToString(), ex.InnerException.ToString(), "ChangePassword", "AdminDashboardController");
+            }
+            return Redirect("/Admin/AdDashboard/ChangePassword");
+        }
+
+    }
+
+   
+}
---
+++ ManPowerSols/Areas/User/Controllers/UserDashboardController.cs
@@ -0,0 +1,173 @@
+﻿using ManPowerSols.Core;
+using ManPowerSols.Core.Models;
+using ManPowerSols.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace ManPowerSols.Areas.User.Controllers
+{
+    public class UserDashboardController : Controller
+    {
+        UserRepository ur = new UserRepository();
+        // GET: User/UserDashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+        public ActionResult ViewProfile()
+        {
+            return View();
+        }
+
+        public ActionResult UserDeals()
+        {
+            return View();
+        }
+
+        public ActionResult Jobs()
+        {
+            return View();
+        }
+
+        public System.Drawing.Image Base64ToImage(string bs64str)
+        {
+            System.Drawing.Image image=null;
+            try
+            {
+                byte[] imageBytes = Convert.FromBase64String(bs64str);
+                MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
+                ms.Write(imageBytes, 0, imageBytes.Length);
+                image = System.Drawing.Image.FromStream(ms, true);
+               
+            }
+            catch(Exception ex)
+            {
+                ErrorWriter.WriteLog(ex.GetType().ToString(), ex.GetType().Name.ToString(), ex.InnerException.ToString(), "GetUserProfile", "UserDashboardController");
+            }
+            return image;
+
+        }
+
+        public class img
+        {
+            public string ImgPath { get; set; }
+        }
+        public JsonResult UpdateUserProfile(UserProfileModel up,img imagpath)
+        {
+            var res = "";
+            try
+            {
+                //if (up.ImgPath != "" && up.ImgPath != null)
+                //{
+                //    if (up.ImgPath.IndexOf(".png") > -1) { }
+                //    else
+                //    {
+                //        Base64ToImage(up.ImgPath).Save(Server.MapPath("~/ProfilePics/" + up.UserID + ".png"));
+                //        up.ImgPath = "/ProfilePics/" + up.UserID + ".png";
+                //    }
+                //}
+                //else
+                //{
+                //    up.ImgPath = "http://mps.manpowersupplier.net/img/admin.png";
+                //}
+                if (imagpath.ImgPath != "" && imagpath.ImgPath != null)
+                {
+                    if (imagpath.ImgPath.IndexOf(".png") > -1) { }
+                    else
+                    {
+                        Base64ToImage(imagpath.ImgPath).Save(Server.MapPath("~/ProfilePics/" + up.UserID + ".png"));
+                        imagpath.ImgPath = "/ProfilePics/" + up.UserID + ".png";
+                    }
+                }
+                else
+                {
+                    imagpath.ImgPath = "http://mps.manpowersupplier.net/img/admin.png";
+                }
+                up.ImgPath = imagpath.ImgPath;
+                string jsr = ur.UpdateUserProfile(up);
+                return Json((new JavaScriptSerializer()).DeserializeObject(jsr), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                ErrorWriter.WriteLog(ex.GetType().ToString(), ex.GetType().Name.ToString(), ex.InnerException.ToString(), "UpdateUserProfile", "UserDashboardController");
+                res = ex.Message.ToString();
+                return Json(res, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult GetUserProfile(GetUserProfileModel up)
+        {
+            var res = "";
+            try
+            {
+                string jsr = ur.GetUserProfile(up);
+                return Json((new JavaScriptSerializer()).DeserializeObject(jsr), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                ErrorWriter.WriteLog(ex.GetType().ToString(), ex.GetType().Name.ToString(), ex.InnerException.ToString(), "GetUserProfile", "UserDashboardController");
+                res = ex.Message.ToString();
+                return Json(res, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult GetUserAppliedJobs(string usrid)
+        {
+            var res = "";
+            try
+            {
+                string jsr = ur.GetUserAppliedJobs(usrid);
+                return Json((new JavaScriptSerializer()).DeserializeObject(jsr), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                ErrorWriter.WriteLog(ex.GetType().ToString(), ex.GetType().Name.ToString(), ex.InnerException.ToString(), "GetUserAppliedJobs", "UserDashboardController");
+                res = ex.Message.ToString();
+                return Json(res, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ChangePassword(FormCollection frm)
+        {
+            ChangePasswordModel cpm = new ChangePasswordModel();
+            MainRepoistory mps = new MainRepoistory();
+            try
+            {
+                cpm.userid = frm["txtuserid"];
+                cpm.usertype = frm["txtusertype"];
+                cpm.password = frm["txtcurrentpwd"];
+                cpm.newpassword = frm["txtnewpwd"];
+                string res = mps.ChangePassword(cpm);
+                JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+                var finalres = JsonConvert.DeserializeObject<ChangePasswordResult>(res);
+                TempData["ResponseCode"] = finalres.ResponseCode;
+                TempData["ResponseMessage"] = finalres.ResponseMessage;
+            }
+            catch(Exception ex)
+            {
+                ErrorWriter.WriteLog(ex.GetType().ToString(), ex.GetType().Name.ToString(), ex.InnerException.ToString(), "ChangePassword", "UserDashboardController");
+            }
+            return Redirect("/User/UserDashboard/ChangePassword");
+        }
+    }
+}
